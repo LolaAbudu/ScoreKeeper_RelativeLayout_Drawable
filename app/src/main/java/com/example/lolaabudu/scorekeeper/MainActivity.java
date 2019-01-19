5582d72e13caac07dc76d26fba6947c2b015ae99
LolaAbudu <lolaabudu1589@gmail.com>
2019-01-18 20:44:03 -0500

    updated project

--- app/src/main/java/com/example/lolaabudu/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/lolaabudu/scorekeeper/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.lolaabudu.scorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mScore1;
+    private int mScore2;
+
+    TextView mScoreTextView1;
+    TextView mScoreTextView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mScoreTextView1 = findViewById(R.id.score_1);
+        mScoreTextView2 = findViewById(R.id.score_2);
+
     }
+
+    public void decreaseScore(View view) {
+        int viewID = view.getId();
+        switch (viewID) {
+            case R.id.decreaseTeam1:
+                mScore1--;
+                mScoreTextView1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.decreaseTeam2:
+                mScore2--;
+                mScoreTextView2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    public void increaseScore(View view) {
+        int viewID = view.getId();
+        switch (viewID){
+            case R.id.increaseTeam1:
+                mScore1++;
+                mScoreTextView1.setText(String.valueOf(mScore1));
+                break;
+            case R.id.increaseTeam2:
+                mScore2++;
+                mScoreTextView2.setText(String.valueOf(mScore2));
+        }
+    }
+
+
 }
